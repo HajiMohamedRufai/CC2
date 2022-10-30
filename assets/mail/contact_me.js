80a5743435b15ff86520eb6840d935eff942db31
HajiMohamedRufai <60290402+HajiMohamedRufai@users.noreply.github.com>
2022-10-30 22:47:24 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://hy8t0wz102.execute-api.eu-west-1.amazonaws.com/default/cc2LambdaFunction",
+                url: "https://7cwafwbhd4.execute-api.eu-west-1.amazonaws.com/default/cc2LambdaFunction",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
